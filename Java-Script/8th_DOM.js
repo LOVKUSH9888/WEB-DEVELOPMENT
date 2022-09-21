333678beaf1e8d5f6d3ef5f7b7e59903227a97f2
LOVKUSH <lovkushyadav9888@gmail.com>
2022-09-21 19:29:35 +0530

    DOM progress

--- Java-Script/8th_DOM.js
+++ Java-Script/8th_DOM.js
@@ -26,4 +26,27 @@ console.log(heading[0]);
 console.log(heading1);
 console.log(heading1[0]); */
 
+// ### Traverse the DOM : Properties
+// 1. parentNode 
+/* const heading = document.querySelector('.heading');
+const parent = heading.parentNode;
+console.log(parent); */ 
 
+//2. childNodes
+/* const parent = document.querySelector('.parent');
+console.log(parent.childNodes); */
+
+//3.nextElementSibling
+/* const heading = document.querySelector('.heading');
+console.log(heading.nextElementSibling); */ 
+
+//3.previousElementSibling
+/* const heading = document.querySelector('h2');
+console.log(heading.previousElementSibling); */ 
+
+
+// #### MAnipulation 
+const heading = document.querySelector('.heading');
+heading.innerHTML = 'Web dev is awesome';
+heading.style.color = 'red';
+heading.style.fontSize = '100px';
