097513427a7ec7ed7dcc84703be5f248356da112
LOVKUSH <lovkushyadav9888@gmail.com>
2022-09-22 15:44:56 +0530

    complete project

--- BULB-ONOFF project/script.js
+++ BULB-ONOFF project/script.js
@@ -1,7 +1,22 @@
+/* const bulbSwitch = document.querySelector('#bulbSwitch');
+const bulb = document.querySelector('#bulb');
+bulbSwitch.addEventListener('click', function(){
+    console.log(bulb.src); //src is its property
+    bulb.src = './bulb-on.gif'; // changing the source here
+}); */
+
+// The abobe code is without the condition statement:
+//Now its more optimize and functional work
+
 const bulbSwitch = document.querySelector('#bulbSwitch');
 const bulb = document.querySelector('#bulb');
 bulbSwitch.addEventListener('click', function(){
     console.log(bulb.src); //src is its property
-    bulb.src = './bulb-on.gif';
+    if(bulb.src.match('off')){
+        bulb.src = './bulb-on.gif';
+        bulbSwitch.innerHTML = 'Turn OFF';
+    }else{
+        bulb.src = './bulb-off.gif';
+        bulbSwitch.innerHTML = 'Turn ON';
+    }
 });
-// The abobe code is without the condition statement: 
