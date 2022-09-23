7f6f05f7504b98c4abdb5ac3805bacba7a44b541
LOVKUSH <lovkushyadav9888@gmail.com>
2022-09-23 16:30:37 +0530

    Array Understanding

---
+++ Java-Script/ARRAY-REF. TYPE/script.js
@@ -0,0 +1,8 @@
+///Storing the collection of multiple items :
+// Ordered collection of Items;
+// 4Lets create an array :
+const friendsName = ["Lovkush", "Shariq", "Vinod"];
+console.log(friendsName);
+console.log(friendsName[0]);
+console.log(friendsName[1]);
+console.log(friendsName[2]);
--- Java-Script/HOISTING/script.js
+++ Java-Script/HOISTING/script.js
@@ -3,16 +3,34 @@
  // TO MOVE OR TO PULL:
 
  // eXAMPLE :
-/* function catName (){
+function catName (){
     console.log("TOM");
  }
- catName(); */
+ catName(); 
 
 
  // By Hoisting this can be also excuted as 
  
- catName();
+catName();
  function catName (){
     console.log("TOM");
- }
+ } 
  
+
+ // var example
+ var valueX; // Declaration
+valueX = 5; // Assignment
+console.log(valueX);  // This will execute properly
+
+ var valueX;
+console.log(valueX);
+valueX = 5; // Like this will show the Undefined Value in the console 
+
+// #### This will also provide the undefined vale its by the JS.
+let teriM;
+console.log(teriM);  
+
+// @@@ But here If we use the strict then we have to declare the values:
+Strict 
+let teriM;
+console.log(teriM);  
