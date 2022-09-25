e73ebca3eac7d62e8d39ae2b965101403ecd268d
LOVKUSH <lovkushyadav9888@gmail.com>
2022-09-25 07:47:45 +0530

    Setting up

--- Java-Script/ARRAY-REF. TYPE/script.js
+++ Java-Script/ARRAY-REF. TYPE/script.js
@@ -1,8 +1,21 @@
 ///Storing the collection of multiple items :
 // Ordered collection of Items;
 // 4Lets create an array :
-const friendsName = ["Lovkush", "Shariq", "Vinod"];
+/* const friendsName = ["Lovkush", "Shariq", "Vinod"];
 console.log(friendsName);
 console.log(friendsName[0]);
 console.log(friendsName[1]);
-console.log(friendsName[2]);
+console.log(friendsName[2]); */
+
+//As array are mutable so: We can change the 
+const friendsName = ["Lovkush", "Shariq", "Vinod"];
+console.log(friendsName);
+friendsName[0] = "Anand"; // As array are mutant so we can change them;
+console.log(friendsName);
+
+
+//Checking type
+console.log(typeof friendsName);
+
+//Checkinh its an Array or not :
+Array.isArray;
