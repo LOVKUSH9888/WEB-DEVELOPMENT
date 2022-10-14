4c0dc0cc2595b9fe6cdb2fa6f713222e29c8c4a6
LOVKUSH <lovkushyadav9888@gmail.com>
2022-10-14 23:34:07 +0530

    Array

---
+++ Java-Script/ARRAY-REF. TYPE/script1.js
@@ -0,0 +1,26 @@
+ let arr = new Array();
+let arr = []; */
+
+let fruits = ["Apple", "Mango", "grapes"];
+console.log(fruits);
+alert(fruits[0]);
+alert(fruits[1]);
+alert(fruits[1]);
+
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars[3]); // this will show undefined automatically
+
+
+///Changing the index of array
+let Marvels = ["IronMan","Thor","Hulk"];
+Marvels[1] = "Loki";
+console.log(Marvels);
+//Checking the type of array 
+console.log(typeof Marvels);
+console.log(Array.isArray(Marvels));
+Array.isArray;
+
