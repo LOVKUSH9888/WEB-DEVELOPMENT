bc072fd88bda571fe9fbc6e9667375d1e972b22b
LOVKUSH <lovkushyadav9888@gmail.com>
2022-10-24 21:29:32 +0530

    completing js part of D&D

---
+++ Drag & Drop Event App/main.js
@@ -0,0 +1,16 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+//fil listeners
+fill.addEventListener('dragstart', dragstart);
+fill.addEventListener('dragend', dragend);
+
+//drag functions 
+function dragStart(){
+    
+
+}
+ function dragEND(){
+
+ }
+
